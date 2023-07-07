6c9a31100a7c246013fc3ad467bb1403b4667eb4
Lynda Weiss <lyndatheengineer@gmail.com>
2023-07-06 19:24:00 -0700

    feature: add function to copy code to clipboard on copy button click

--- code-copy.js
+++ code-copy.js
@@ -1,9 +1,20 @@
 function startCodeCopy() {
   const copyButtons = document.querySelectorAll('.copy-button');
   const copyCodeContainers = document.querySelectorAll('.can-copy');
+
+  const copyCodeToClipboard = async index => {
+    const copiedCode = copyCodeContainers[index].textContent;
+
+    try {
+      await navigator.clipboard.writeText(copiedCode);
+      console.log('copied');
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
-  copyButtons.forEach((btn, index) => btn.addEventListener('click', () => {
-    console.log(`Clicked button ${index}`)
+  copyButtons.forEach((btn, index) => btn.addEventListener('click', async () => {
+    copyCodeToClipboard(index);
   }));
 }
 
